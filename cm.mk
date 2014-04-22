ce9952c91a0e74527819cb98f3bc551a4e2d02b8
Alberto96 <albertoguanti@gmail.com>
2014-04-22 22:19:41 +0200

    tassve: cm.mk: clean-up

--- cm.mk
+++ cm.mk
@@ -1,3 +1,9 @@
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := TassVE
+
 # BootAnimation
 TARGET_BOOTANIMATION_NAME := vertical-240x320
 
@@ -7,16 +13,14 @@ $(call inherit-product, device/samsung/tassve/full_tassve.mk)
 # Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
-# Include GSM stuff
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-# Release name and versioning
-PRODUCT_RELEASE_NAME := Galaxytassve
-PRODUCT_VERSION_DEVICE_SPECIFIC := -GT-S5570i
-
-## Device identifier. This must come after all inclusions
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tassve
 PRODUCT_NAME := cm_tassve
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := GT-S5570i
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-S5570i TARGET_DEVICE=GT-S5570i BUILD_FINGERPRINT=samsung/GT-S5570I/GT-S5570I:2.3.6/GINGERBREAD/XIMJ1:user/release-keys PRIVATE_BUILD_DESC="GT-S5570I-user 2.3.6 GINGERBREAD XIMJ1 release-keys"
